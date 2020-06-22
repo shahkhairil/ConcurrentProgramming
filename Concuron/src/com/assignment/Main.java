55bce0bd5ab19b91b588c745ff030a18b1caf053
firdausly <50550275+firdausly@users.noreply.github.com>
2020-06-22 15:30:37 +0800

    change package name

---
+++ Concuron/src/com/assignment/Main.java
@@ -0,0 +1,63 @@
+package com.assignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    //generate random point object until n times
+    static Point[] generateRandomPoint(int n) {
+        //create new Point object array with size of n
+        Point[] x=new Point[n];
+        //intialize random g
+        Random g=new Random() ;
+
+        //loop until n times
+        for(int i=0;i<n;i++){
+            //generate random float point in 1000*1000 region
+            float xcoord=g.nextFloat()*1000;
+            float ycoord=g.nextFloat()*1000;
+
+            //add the random float point into Point object
+            x[i]=new Point(xcoord,ycoord);
+        }
+        //return Point object array
+    return x;
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // create an object of Scanner
+        Scanner input = new Scanner(System.in);
+
+        // take input from the user
+        System.out.print("Enter n: ");
+        int n = input.nextInt();
+
+        Point[] arrayofpoint=generateRandomPoint(n);
+
+
+        for(int i=0;i<arrayofpoint.length;i++){
+            System.out.println(arrayofpoint[i]);
+        }
+
+        generateEdge edge=new generateEdge(arrayofpoint);
+
+        System.out.print("Enter t: ");
+        int t = input.nextInt();
+
+        Thread thread[] = new Thread[t];
+        for (int i = 0; i < t; i++)
+        {
+            thread[i] = new Thread(new EdgeJob(edge), "Thread " + i);
+        }
+        for (int i = 0; i < t; i++)
+        {
+            thread[i].start();
+        }
+
+
+    }
+}
---
+++ Concuron/src/com/assignment/Point.java
@@ -0,0 +1,46 @@
+package com.assignment;
+
+public class Point {
+    private float x;
+    private float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+    }
+
+    public Point(int n){
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void getCoordinate(){
+        System.out.println(this.x+","+this.y);
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
