badf636685d9a1da641d92073afc02d95a36d587
firdausly <50550275+firdausly@users.noreply.github.com>
2020-06-22 15:06:11 +0800

    concurren

---
+++ Concuron/src/com/company/EdgeJob.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class EdgeJob implements Runnable {
+    private generateEdge generateedge;
+
+    public EdgeJob(generateEdge generateedge) {
+        this.generateedge = generateedge;
+    }
+
+    @Override
+    public void run() {
+
+//        System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
+        generateedge.generate();
+    }
+}
